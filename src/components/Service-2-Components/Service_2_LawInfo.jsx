3be0874f5c718dd9c84c4b1931d4782aa3ad255f
Rakibul Hasan <rhrumon@gmail.com>
2025-01-29 09:59:35 +0600

    something ok

--- src/components/Service-2-Components/Service_2_LawInfo.jsx
+++ src/components/Service-2-Components/Service_2_LawInfo.jsx
@@ -1,3 +1,4 @@
+import jsPDF from "jspdf";
 import "../../../src/style/Style.css";
 import { AiOutlineFilePdf } from "react-icons/ai";
 
@@ -6,6 +7,12 @@ import { AiOutlineFilePdf } from "react-icons/ai";
 const Service_2_LawInfo = ({info}) => {
 
     let {number, title, description1, description2} = info;
+
+    const handleDownloadPDF = () => {
+      const doc = new jsPDF();
+      doc.text("Hello, its a example of pdf", 8, 8);
+      doc.save("example.pdf");
+    };
 
     return (
         <div className="flex big-mid:flex-row flex-col justify-between items-start big-mid:items-center md:mt-[40px] mt-[18px]">
@@ -24,7 +31,7 @@ const Service_2_LawInfo = ({info}) => {
             </div>
 
             <div className="big-mid:pl-0 pl-2 big-mid:mt-0 mt-4">
-              <button className="bg-[#B68C5A] rounded-lg font-medium whitespace-nowrap lg:max-w-[228px] md:max-w-[190px] sm:max-w-[160px] max-w-[142px] lg:h-[56px] md:h-[50px] sm:h-[44px] h-[40px] lg:text-lg md:text-base sm:text-sm text-xs flex items-center justify-center md:gap-[10px] gap-[6px] text-white border lg:px-8 md:px-6 sm:px-4 px-3 lg:py-4 md:py-3 sm:py-2 py-1 work-sans">
+              <button onClick={() => handleDownloadPDF()} className="primary-btn">
                 <AiOutlineFilePdf className="lg:h-6 lg:w-6 md:h-5 md:w-5 sm:h-4 sm:w-4 h-3 w-3" />
                 Download PDF
               </button>
